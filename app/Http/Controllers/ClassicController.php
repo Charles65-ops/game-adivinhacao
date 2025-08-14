681714356912885d3b2ce305902b870091400ee1
Charles <charles@gmail.com>
2025-08-13 21:21:11 -0300

    Att

--- app/Http/Controllers/ClassicController.php
+++ app/Http/Controllers/ClassicController.php
@@ -17,14 +17,97 @@ class ClassicController extends Controller
         $dificuldade = $request->dificuldade;
         $tentativas = $request->attemps ?? 0;
 
-        $caminho = storage_path("app/public/python.png");
+        $caminho = storage_path("app/public/elixir.png");
 
         if(!file_exists($caminho)){
             abort(404, "Imagem não encontrada.");
 
     }
 
-    echo "Deu certo";
+    $extensao = strtolower(pathinfo($caminho, PATHINFO_EXTENSION));
+
+    if(!in_array($extensao, ['jpg', 'png', 'jpeg'])){
+        abort(415, "Formato de imagem não suportada.");
+    }
+    $imagemOriginal = match ($extensao){
+        'png' => imagecreatefrompng($caminho),
+        default => imagecreatefromjpeg($caminho),
+
+    };
+
+    //PIXELIZAÇÂO - NIVEL 1
+    $largura = imagesx($imagemOriginal);
+    $altura = imagesy($imagemOriginal);
+
+    $fatorPixelizacao = ((5 + $tentativas) / 100)- 0.02;
+
+    $novaLargura = max(1, intval($largura * $fatorPixelizacao));
+    $novaAltura = max(1, intval($altura * $fatorPixelizacao));
+
+    $imagemPequena = imagecreatetruecolor($novaLargura, $novaAltura);
+    imagecopyresized($imagemPequena,$imagemOriginal, 
+    0, 0, 0, 0, 
+    $novaLargura, 
+    $novaAltura, 
+    $largura, 
+    $altura
+);
+
+$imagemFinal = imagecreatetruecolor($largura, $altura);
+imagecopyresized($imagemFinal,$imagemPequena,
+0 ,0 ,0 ,0,
+$largura,
+$altura,
+$novaAltura,
+$novaLargura,
+);
+
+//RUIDO - NIVEL 2
+$tamanhoRudio = ((5 - $tentativas) * 10)/2;
+for($i = 0; $i < 150; $i++){
+    $x = rand(0, $largura - $tamanhoRudio);
+    $y = rand(0, $altura - $tamanhoRudio);
+    $cor = imagecolorallocate($imagemFinal, 
+    rand(0,255), 
+    rand(0,255), 
+    rand(0,   255)
+);
+
+
+imagefilledrectangle(
+    $imagemFinal,
+    $x,
+    $y,
+    $x + $tamanhoRudio,
+    $y + $tamanhoRudio,
+    $cor
+);
+}
+
+//PRETO E BRANCO - 3
+imagefilter($imagemFinal, IMG_FILTER_GRAYSCALE);
+
+    ob_start();
+
+    if($extensao == "png"){
+        imagepng($imagemFinal);
+        $contentType = "image/png";
+    }else {
+        imagejpeg($imagemFinal);
+        $contentType = "image/png";
+    }
+
+    $conteudoImagem = ob_get_clean();
+    imagedestroy($imagemFinal);
+
+    $base64 = 'data' . $contentType . ';base64,' . base64_encode($conteudoImagem);
+    return response()->json([
+        "image" => "exemplo",
+        "dificuldade" => "Teste",
+        "resposta" => "a",
+        "vidas" => "a"
+    ]);
     
  }
+
 }
